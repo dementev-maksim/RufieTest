e1e37fc73df8ce737adcd365192aef7590de43e7
Dementev Maksim <97791397+dementev-maksim@users.noreply.github.com>
2022-05-25 20:12:13 +0300

    ДЛЯ УРОКА!!! СКАЧАТЬ ЭТО!

---
+++ my_app.py
@@ -0,0 +1,41 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui
+from instr import *
+from second_win import *
+from final_win import *
+
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self): 
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self): 
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.btn_next = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.btn_next)
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
